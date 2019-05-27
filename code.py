87b824afaa7137b150aae8de7e8baa2efd631917
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-05-27 12:34:53 -0600

    feat: convert to data set

--- code.py
+++ code.py
@@ -847,3 +847,9 @@ df_delay_weather_1
 df_delay_NAS_0 = df_2008s.dropna(subset=['NASDelay'])
 df_delay_NAS_0 = df_delay_NAS_0[df_delay_NAS_0['NASDelay']!=0]
 df_delay_NAS_1 = df_delay_NAS_0.groupby(['Month'])['NASDelay'].mean()
+
+
+# In[118]:
+# convert to data set
+df_delay_NAS_1 = df_delay_NAS_1.reset_index()
+df_delay_NAS_1
